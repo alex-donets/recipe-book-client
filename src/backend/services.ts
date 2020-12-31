214206cc159df85571ae31962743cd4380e516e4
Veronika Donets <veronikad@play-consult.co.uk>
2020-12-31 02:15:10 +0200

    fix console

--- src/backend/services.ts
+++ src/backend/services.ts
@@ -4,12 +4,12 @@ import io from 'socket.io-client';
 export const apiUrl =
     process.env.NODE_ENV === 'development' ? process.env.REACT_APP_API_URL_DEV : process.env.REACT_APP_API_URL_PROD;
 
-console.log('apiUrl', apiUrl);
-
 export const uiUrl =
     process.env.NODE_ENV === 'development' ? process.env.REACT_APP_UI_URL_DEV : process.env.REACT_APP_UI_URL_PROD;
 
+console.log('ENVIRONMENT: ', process.env.NODE_ENV);
 console.log('uiUrl', uiUrl);
+console.log('apiUrl', apiUrl);
 
 export const apiClient = axios.create({
     baseURL: process.env.REACT_APP_API_URL_DEV,
